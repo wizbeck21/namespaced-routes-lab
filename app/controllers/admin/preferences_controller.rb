e22de05f629281082239d820ba418694d5c0806c
Wesley <wes.beck21@outlook.com>
2021-01-25 15:43:41 -0800

    commit tree 069cdad99cb4668dda550efcaff16661daa68f3a

---
+++ app/controllers/admin/preferences_controller.rb
@@ -0,0 +1,8 @@
+class Admin::PreferencesController < ApplicationController
+
+  def index
+
+  end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   end
   resources :songs
 
+  namespace :admin do
+    resources :preferences
+  end
 end
